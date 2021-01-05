b2fe2a5ddc554c6e83601d653ade86b258ee96b0
Aszalea Calderon <aszalea_calderon@yahoo.com>
2021-01-05 10:37:17 -0800

    running????

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React YAY
-        </a>
-      </header>
+      <Route exact path="/" component={Home} />
+      <Route path="/Signup" component={Signup} />
+      <Route path="/Login" component={Login} />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "../assets/potluck5.jpg";
+import Logo from "./Logo";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div>
+      <nav>
+        <Logo />
+        <div className="links">
+          <Link to="/" className="link">
+            Home
+          </Link>
+          <Link to="Signup" className="link">
+            Signup
+          </Link>
+        </div>
+      </nav>
+      <section className="centering">
+        <img src={Image} className="logins" alt="fruits"></img>
+        <section className="signupLogin">
+          <h2 className="signupLoginHeader">Login</h2>
+          <form className="signupLoginform">
+            <input placeholder="Username" type="text"></input>
+            <input placeholder="Password" type="text"></input>
+            <button>Login</button>
+          </form>
+          <Link to="Signup" className="signupLoginLink">
+            Don't have an account? Sign up!asd
+          </Link>
+        </section>
+      </section>
+    </div>
+  );
+};
+
+export default Login;
