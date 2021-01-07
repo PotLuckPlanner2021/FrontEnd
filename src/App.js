0aa143ff85cb96dd3a15a646c8e28baaaa3e3b6f
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-01-07 06:55:51 -0700

    Working on Event List first

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Items from "./Items"
 import PartyForm from "./components/PartyForm"
-import { Route } from "react-router-dom";
+import EventList from './components/EventList'
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={Home} />
-      <Route path="/Signup" component={Signup} />
-      <Route path="/Login" component={Login} />
-      <Route path="/Items" component={Items} />
-      <Route path="/PartyForm" component={PartyForm} />
+      <EventList />
+      <Switch>
+        <Route path="/Signup" component={Signup} />
+        <Route path="/Login" component={Login} />
+        <Route path="/Items" component={Items} />
+        <Route path="/NewPotluck" component={PartyForm} />
+        <Route path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/EventCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EventCard = () => {
+
+  return (
+    <div>
+      <h4>Test</h4>
+    </div>
+  )
+};
+
+export default EventCard;
